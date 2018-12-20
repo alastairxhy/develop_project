6a5baa311b22083bc45228c8ae348a451a5398db
Administrator <1057771469@qq.com>
2018-12-20 15:06:42 +0800

    修改样式

---
+++ websource/js/monitor_platform_js/config.js
@@ -0,0 +1,31 @@
+var develop = {
+    debug:true,
+};
+var request_url, request_url_json ;
+
+
+if(develop.debug){
+    request_url = 'http://admincs.yunandawulian.com/appServer/webApi/rpv1';
+    request_url_json =  'http://admincs.yunandawulian.com/appServer/webApi/rbv1';
+}else {
+    request_url = 'http://admin.yunandawulian.com/webApi/rpv1';
+    request_url_json =  'http://admin.yunandawulian.com/webApi/rbv1';
+};
+
+$.ajaxSetup({
+    url:request_url,
+    type:"post",
+    dataType:"json",
+    xhrFields:{withCredentials: true},
+    complete: function (XMLHttpRequest, textStatus, xhr) {
+        var oauthstatus = XMLHttpRequest.getResponseHeader("oauthstatus");
+        if (oauthstatus == '401') {
+            self.location.href = "../../../login2.html";
+        }
+
+    },
+});
+
+var  menus_data = {
+
+};
