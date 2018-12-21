3605e12507bf1182ad0eb4506ad7f8e3cedf7138
Administrator <1057771469@qq.com>
2018-12-21 17:00:03 +0800

    修改首页样式

--- websource/js/monitor_platform_js/config.js
+++ websource/js/monitor_platform_js/config.js
@@ -1,22 +1,23 @@
 var develop = {
-    debug:true,
+    debug: true,
 };
-var request_url, request_url_json ;
+var request_url, request_url_json;
 
 
-if(develop.debug){
+if (develop.debug) {
     request_url = 'http://admincs.yunandawulian.com/appServer/webApi/rpv1';
-    request_url_json =  'http://admincs.yunandawulian.com/appServer/webApi/rbv1';
-}else {
+    request_url_json = 'http://admincs.yunandawulian.com/appServer/webApi/rbv1';
+} else {
     request_url = 'http://admin.yunandawulian.com/webApi/rpv1';
-    request_url_json =  'http://admin.yunandawulian.com/webApi/rbv1';
-};
+    request_url_json = 'http://admin.yunandawulian.com/webApi/rbv1';
+}
+;
 
 $.ajaxSetup({
-    url:request_url,
-    type:"post",
-    dataType:"json",
-    xhrFields:{withCredentials: true},
+    url: request_url,
+    type: "post",
+    dataType: "json",
+    xhrFields: {withCredentials: true},
     complete: function (XMLHttpRequest, textStatus, xhr) {
         var oauthstatus = XMLHttpRequest.getResponseHeader("oauthstatus");
         if (oauthstatus == '401') {
@@ -26,6 +27,6 @@ $.ajaxSetup({
     },
 });
 
-var  menus_data = {
+var menus_data = {
 
 };
